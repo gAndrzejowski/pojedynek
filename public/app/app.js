1f5ca8e20c2c7392b5dc3b123fbe815b5afd6b0b
Andrzejowski <g.andrzejowski@gmail.com>
2016-02-11 00:15:36 +0100

    selecting cards in new deal (POST)

--- public/app/app.js
+++ public/app/app.js
@@ -80,6 +80,153 @@ angular.module('contestApp',['routerRoutes'])
                 con.diffMsgClass = 'alert-success'
             }
     }
+    con.parseSuit = function(suitArray){
+        var suitString = '';
+        for (i=0;i<suitArray.length;i++)
+        {
+            if (suitArray[i][0]) suitString += suitArray[i][1];
+        }
+        return suitString;
+    }
+    con.checkCards = function(hand) {
+        var cards, pClass;
+        if (hand=='W') cards = con.parseSuit(con.WSpadesOption).length+con.parseSuit(con.WHeartsOption).length+con.parseSuit(con.WDiamsOption).length+con.parseSuit(con.WClubsOption).length;
+        else cards = con.parseSuit(con.ESpadesOption).length+con.parseSuit(con.EHeartsOption).length+con.parseSuit(con.EDiamsOption).length+con.parseSuit(con.EClubsOption).length;    
+        if (cards<13) pClass = "text-warning";
+        else if (cards==13) pClass = "text-success";
+        else pClass = "text-danger";
+        var pMessage = '<p class="'+pClass+'">Liczba kart '+hand+': '+cards+'</p>';
+        return $sce.trustAsHtml(pMessage);
+    }
+    con.WSpadesOption = 
+             [
+             [false,'A'],
+             [false,'K'],
+             [false,'D'],
+             [false,'W'],
+             [false,'10'],
+             [false,'9'],
+             [false,'8'],
+             [false,'7'],
+             [false,'6'],
+             [false,'5'],
+             [false,'4'],
+             [false,'3'],
+             [false,'2'],
+             ];
+    con.WHeartsOption = 
+             [
+             [false,'A'],
+             [false,'K'],
+             [false,'D'],
+             [false,'W'],
+             [false,'10'],
+             [false,'9'],
+             [false,'8'],
+             [false,'7'],
+             [false,'6'],
+             [false,'5'],
+             [false,'4'],
+             [false,'3'],
+             [false,'2'],
+             ];    
+     con.WDiamsOption = 
+             [
+             [false,'A'],
+             [false,'K'],
+             [false,'D'],
+             [false,'W'],
+             [false,'10'],
+             [false,'9'],
+             [false,'8'],
+             [false,'7'],
+             [false,'6'],
+             [false,'5'],
+             [false,'4'],
+             [false,'3'],
+             [false,'2'],
+             ];
+    con.WClubsOption = 
+             [
+             [false,'A'],
+             [false,'K'],
+             [false,'D'],
+             [false,'W'],
+             [false,'10'],
+             [false,'9'],
+             [false,'8'],
+             [false,'7'],
+             [false,'6'],
+             [false,'5'],
+             [false,'4'],
+             [false,'3'],
+             [false,'2'],
+             ];
+    con.ESpadesOption = 
+             [
+             [false,'A'],
+             [false,'K'],
+             [false,'D'],
+             [false,'W'],
+             [false,'10'],
+             [false,'9'],
+             [false,'8'],
+             [false,'7'],
+             [false,'6'],
+             [false,'5'],
+             [false,'4'],
+             [false,'3'],
+             [false,'2'],
+             ];
+    con.EHeartsOption = 
+             [
+             [false,'A'],
+             [false,'K'],
+             [false,'D'],
+             [false,'W'],
+             [false,'10'],
+             [false,'9'],
+             [false,'8'],
+             [false,'7'],
+             [false,'6'],
+             [false,'5'],
+             [false,'4'],
+             [false,'3'],
+             [false,'2'],
+             ];
+    con.EDiamsOption = 
+             [
+             [false,'A'],
+             [false,'K'],
+             [false,'D'],
+             [false,'W'],
+             [false,'10'],
+             [false,'9'],
+             [false,'8'],
+             [false,'7'],
+             [false,'6'],
+             [false,'5'],
+             [false,'4'],
+             [false,'3'],
+             [false,'2'],
+             ];
+    con.EClubsOption = 
+             [
+             [false,'A'],
+             [false,'K'],
+             [false,'D'],
+             [false,'W'],
+             [false,'10'],
+             [false,'9'],
+             [false,'8'],
+             [false,'7'],
+             [false,'6'],
+             [false,'5'],
+             [false,'4'],
+             [false,'3'],
+             [false,'2'],
+             ];
+    
     con.presentMe = 'I take care of single deal actions';
 })
 // controller to take care of creating and presenting contests
